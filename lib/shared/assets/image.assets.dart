ddfe4bfa840e2d6aecf950df8c6ca9cf9a8bd3ee
Hanzla <hanzlaali42372@gmail.com>
2023-03-27 11:38:22 +0500

    initial comit

---
+++ lib/shared/assets/image.assets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ImageAssets {
+  ImageAssets._();
+
+  static const AssetImage img300 = AssetImage('assets/images/300.png');
+  static const AssetImage img400 = AssetImage('assets/images/400.png');
+}
+
+class JsonAssets {
+  JsonAssets._();
+
+  static const String gpayAsset =
+      'json/default_payment_profile_google_pay.json';
+}
